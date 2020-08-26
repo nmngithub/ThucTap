fce8d5c28d5fe48204ba16e1bddef835963db413
Nguyen Minh Nghia <>
2020-08-26 15:34:54 +0700

    Update
    Update

---
+++ Project-THP/js/index.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+ 
+           
+           
+            // <div class="thoisu p2">
+            //     <div class="time">7:00</div>
+            //     <div class="thoisu-content">Thời sự</div>
+            //     <div class="tentin">Bản tin thời sự ngày</div>
+            // </div>
+            // <div class="thoisu p3">
+            //     <div class="time">10:00</div>
+            //     <div class="live">Live</div>
+            //     <div class="thoisu-content">Thời sự</div>
+            //     <div class="tentin">Bản tin thời sự ngày</div>
+            // </div>
+            // <div class="thoisu p4">
+            //     <div class="time">7:00</div>
+            //     <div class="thoisu-content">Thời sự</div>
+            //     <div class="tentin">Bản tin thời sự ngày</div>
+            // </div>
+            // <div class="thoisu p5">
+            //     <div class="time">7:00</div>
+            //     <div class="thoisu-content">Thời sự</div>
+            //     <div class="tentin">Bản tin thời sự ngày</div>
+            // </div>
+            // <div class="thoisu p6">
+            //     <div class="time">7:00</div>
+            //     <div class="thoisu-content">Thời sự</div>
+            //     <div class="tentin">Bản tin thời sự ngày</div>
+            // </div>
+            // <div class="thoisu p7">
+            //     <div class="time">7:00</div>
+            //     <div class="thoisu-content">Thời sự</div>
+            //     <div class="tentin">Bản tin thời sự ngày</div>
+            // </div>
+            // <div class="thoisu p8">
+            //     <div class="time">7:00</div>
+            //     <div class="thoisu-content">Thời sự</div>
+            //     <div class="tentin">Bản tin thời sự ngày</div>
+            // </div>
+            // <div class="thoisu p9">
+            //     <div class="time">7:00</div>
+            //     <div class="thoisu-content">Thời sự</div>
+            //     <div class="tentin">Bản tin thời sự ngày</div>
+            // </div>
+            // <div class="thoisu p10">
+            //     <div class="time">7:00</div>
+            //     <div class="thoisu-content">Thời sự</div>
+            //     <div class="tentin">Bản tin thời sự ngày</div>
+            // </div>
+            // <div class="thoisu p11">
+            //     <div class="time">7:00</div>
+            //     <div class="thoisu-content">Thời sự</div>
+            //     <div class="tentin">Bản tin thời sự ngày</div>
+            // </div>
+})
